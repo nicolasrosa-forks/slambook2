d35bf1d21fa6e995a44e0a0803d3d5aa21d51427
nicolasrosa <nicolas.rosa@hotmail.com>
2020-11-26 19:11:51 -0300

    [MOD] useGeometry created

---
+++ nicolas/ch3/useGeometry/useGeometry.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+void print(char var[]){
+    cout << var << endl;
+}
+
+int main(int argc, char **argv){
+    print("hello");
+
+    return 0;
+}
