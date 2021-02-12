94a0e4cb0c8bf7a24c5f394b5d8e4bce603b7c11
nicolasrosa <nicolas.rosa@hotmail.com>
2021-02-11 21:49:16 -0300

    [Mod] Chapter 6 gaussNewton updated 2

---
+++ nicolas/ch6/optimization/include/gaussNewton.h
@@ -0,0 +1,34 @@
+/* System Libraries */
+#include <iostream>
+#include <cmath>
+#include <chrono>
+#include <vector>
+
+/* Eigen3 Libraries */
+#include <eigen3/Eigen/Core>
+#include <Eigen/Dense>
+
+/* Pangolin Library */
+// #include <pangolin/pangolin.h>
+
+/* OpenCV Library */
+#include <opencv2/opencv.hpp>
+// #include <opencv2/core/core.hpp>
+
+/* Ceres Library */
+// #include <ceres/ceres.h>
+
+/* g2o Library */
+// #include <g2o/core/g2o_core_api.h>
+// #include <g2o/core/base_vertex.h>
+// #include <g2o/core/base_unary_edge.h>
+// #include <g2o/core/block_solver.h>
+// #include <g2o/core/optimization_algorithm_levenberg.h>
+// #include <g2o/core/optimization_algorithm_gauss_newton.h>
+// #include <g2o/core/optimization_algorithm_dogleg.h>
+// #include <g2o/solvers/dense/linear_solver_dense.h>
+
+/* Custom Libraries */
+#include "../../include/libUtils.h"
+
+/* Function Scopes */
