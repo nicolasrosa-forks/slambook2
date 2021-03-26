6e6120e406b00ab381d28e7bcab3ed3cd9c85b07
nicolasrosa <nicolas.rosa@hotmail.com>
2021-03-26 19:27:54 -0300

    [Mod] bundleAdjustmentGaussNewton fixed!

--- nicolas/ch7/include/bundleAdjustment.cpp
+++ nicolas/ch7/include/bundleAdjustment.cpp
@@ -32,13 +32,24 @@
 using namespace std;
 using namespace cv;
 
-typedef Eigen::Matrix<double, 6, 1> Vector6d;
-
+/* Global Variables */
 // The memory is aligned as for dynamically aligned matrix/array types such as MatrixXd
+typedef Eigen::Matrix<double, 6, 1> Vector6d;
 typedef vector<Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d>> VecVector3d;
 typedef vector<Eigen::Vector2d, Eigen::aligned_allocator<Eigen::Vector2d>> VecVector2d;
 
+// Choose the optimization algorithm:
+// 1: Gauss-Newton, 2: Levenberg-Marquardt, 3: Powell's Dog Leg
+int optimization_algorithm_selected = 1;
+
+/* =========== */
+/*  Functions  */
+/* =========== */
 void bundleAdjustmentGaussNewton(const VecVector3d &points_3d, const VecVector2d &points_2d, const Mat &K, Sophus::SE3d &pose){
+    cout << "| ----------------------- |" << endl;
+    cout << "|  Bundle Adjustment (GN) |" << endl;
+    cout << "| ----------------------- |" << endl;
+
     /* Initialization */
     const int iterations = 10;
     double cost = 0.0, lastCost = 0.0;
@@ -46,10 +57,6 @@ void bundleAdjustmentGaussNewton(const VecVector3d &points_3d, const VecVector2d
     double fy = K.at<double>(1, 1);
     double cx = K.at<double>(0, 2);
     double cy = K.at<double>(1, 2);
-
-    cout << "| ----------------------- |" << endl;
-    cout << "|  Bundle Adjustment (GN) |" << endl;
-    cout << "| ----------------------- |" << endl;
 
     /* Iteration Loop */
     print("Summary: ");
@@ -63,40 +70,42 @@ void bundleAdjustmentGaussNewton(const VecVector3d &points_3d, const VecVector2d
         /* Data Loop, Compute Cost */
         for (int i = 0; i < points_3d.size(); i++){
             // Describe the 3D Space Point P in the {camera2} frame
-            Eigen::Vector3d Pc2 = pose * points_3d[i]; // P'_i = (T*P_i)1:3, P2_i = T21*P1_i
-
-            double inv_z = 1.0 / Pc2[2];   // 1/Z
-            double inv_z2 = inv_z * inv_z; // 1/(Z^2)
+            Eigen::Vector3d P2 = pose * points_3d[i]; // P'_i = (T*P_i)1:3, P2_i = T21*P1_i
 
             /* ----- Compute Reprojection Error ----- */
             // Compute the Estimated Projection of P in Camera 2 (Pixel Coordinates)
-            Eigen::Vector2d proj(fx * Pc2[0] / Pc2[2] + cx, fy * Pc2[1] / Pc2[2] + cy); //p2^=[u, v]^T = [fx*X/Z+cx, fy*Y/Z+cy]^T
+            Eigen::Vector2d proj(fx * P2[0] / P2[2] + cx, fy * P2[1] / P2[2] + cy); //p2^=[u2, v2]^T = [fx*X/Z+cx, fy*Y/Z+cy]^T
 
             // Compute Residual
             Eigen::Vector2d e = points_2d[i] - proj; // e = p2_i - p2^_i
 
-            /* ----- Jacobians ----- */
-            // Compute the Jacobian Matrix
-            Eigen::Matrix<double, 2, 6> J;  // 
-            J << -fx * inv_z,
+            /* ----- Compute Jacobian Matrix ----- */
+            // The jacobian matrix indicates how the error varies according to the increment δξ, ∂e/∂δξ
+            double inv_Z = 1.0 / P2[2];   // 1/Z
+            double inv_Z2 = inv_Z * inv_Z; // 1/(Z^2)
+
+            Eigen::Matrix<double, 2, 6> J;
+            J << -fx * inv_Z, 
                 0,
-                fx * Pc2[0] * inv_z2,
-                fx * Pc2[0] * Pc2[1] * inv_z2,
-                -fx - fx * Pc2[0] * Pc2[0] * inv_z2,
-                fx * Pc2[0] * Pc2[1] * inv_z,
+                fx * P2[0] * inv_Z2,
+                fx * P2[0] * P2[1] * inv_Z2,
+                -fx - fx * P2[0] * P2[0] * inv_Z2,
+                fx * P2[1] * inv_Z,
                 0,
-                -fy * inv_z,
-                fy * Pc2[1] * inv_z2,
-                fy + fy * Pc2[1] * Pc2[1] * inv_z2,
-                -fy * Pc2[0] * Pc2[1] * inv_z2,
-                -fy * Pc2[0] * inv_z;
+                -fy * inv_Z,
+                fy * P2[1] * inv_Z2,
+                fy + fy * P2[1] * P2[1] * inv_Z2,
+                -fy * P2[0] * P2[1] * inv_Z2,
+                -fy * P2[0] * inv_Z;
+
 
             /* ------ Hessian and Bias ----- */
-            H += J.transpose() * J;  // Hessian, H(x) = J(x)'*Ω*J(x)
-            b += -J.transpose() * e; // Bias, g(x) = -b(x) = -Ω*J(x)*f(x), f(x)=e(x)
+            H +=  J.transpose() * J;  // Hessian, H(x) = J(x)'*Ω*J(x)
+            b += -J.transpose() * e;  // Bias, g(x) = -b(x) = -Ω*J(x)*f(x), f(x)=e(x)
 
             // Least-Squares Cost (Objective Function)
-            cost += e.squaredNorm();  // The actual error function being minimized by solving the proposed linear system is min_x(sum_i ||ei(x)||^2).
+            // This is the actual error function being minimized by solving the proposed linear system: min_x(sum_i ||ei(x)||^2).
+            cost += e.squaredNorm();  // Summation of the squared residuals
         }
 
         /* ----- Solve ----- */
@@ -111,18 +120,21 @@ void bundleAdjustmentGaussNewton(const VecVector3d &points_3d, const VecVector2d
 
         /* Stopping Criteria */
         // If converged or the cost increased, the update was not good, then break.
-        if (dx.norm() < 1e-6 || (iter > 0 && cost >= lastCost)){
-            cout << "\ncost: " << cost << " >= lastCost: " << lastCost << ", break!" << endl;
+        if (iter > 0 && cost >= lastCost){
+            cout << "\ncost: " << std::setprecision(12) << cost << " >= lastCost: " << lastCost << ", break!" << endl;
             break;
         }
 
         /* ----- Update ----- */
-        // Left multiply T by a disturbance quantity δξ
-        pose = Sophus::SE3d::exp(dx) * pose; // T* = exp(δξ).T
-        
+        // Left multiply T by a disturbance quantity exp(δξ)
+        pose = Sophus::SE3d::exp(dx) * pose;  // T* = exp(δξ).T
         lastCost = cost;
-        // cout << "it: " << iter << "cost: " << cost << ", \tupdate: " << dx.transpose() << "\t\testimated params: " << pose.matrix() << endl;
+        
         cout << "it: " << iter << ",\tcost: " << cost << ",\tupdate: " << dx.transpose() << endl;
+        if (dx.norm() < 1e-6){
+            //converge
+            break;
+    }
     }
     Timer t2 = chrono::steady_clock::now();
 
