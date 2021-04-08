a535a59c6fcbbdbaee37652c917476134232cb2c
nicolasrosa <nicolas.rosa@hotmail.com>
2021-04-08 14:39:09 -0300

    [Mod] SIFT, FLANN, Lowe's ratio test added

--- nicolas/ch7/include/find_features_matches.cpp
+++ nicolas/ch7/include/find_features_matches.cpp
@@ -1,24 +1,20 @@
-/** Features
- * @ORB: https://docs.opencv.org/master/df/dd2/tutorial_py_surf_intro.html
- * @SURF: https://docs.opencv.org/master/df/dd2/tutorial_py_surf_intro.html
- * 
- */
-
 /* =========== */
 /*  Libraries  */
 /* =========== */
-#define OPENCV3  // If not defined, OpenCV2
-
 /* System Libraries */
 #include <iostream>
 #include <chrono>
+#include <string>
 
 /* OpenCV Libraries */
 #include <opencv2/core/core.hpp>
+#include <opencv2/opencv.hpp>
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
-#include <opencv2/opencv.hpp>
+
+// #ifdef HAVE_OPENCV_XFEATURES2D
+// #include <opencv2/xfeatures2d/xfeatures2d.hpp>
 
 /* Custom Libraries */
 #include "../../common/libUtils_basic.h"
@@ -27,38 +23,98 @@
 
 using namespace std;
 using namespace cv;
+// using namespace cv::xfeatures2d;
 
-/* ==================== */
-/*  OpenCV's Functions  */
-/* ==================== */
+/** Features Descriptors
+ * Binary-string descriptors: ORB, BRIEF, BRISK, FREAK, AKAZE, etc.
+ * Floating-point descriptors: SIFT, SURF, GLOH, etc.
+ * 
+ * @ORB:  Free, 
+ *  - https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_orb/py_orb.html
+ * @SIFT: Paid, 
+ *  - https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_sift_intro/py_sift_intro.html
+ * @SURF: Paid, 
+ *  - https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_surf_intro/py_surf_intro.html
+ *  - https://docs.opencv.org/3.4/d7/d66/tutorial_feature_detection.html
+
+/** Features Matching
+ * Feature matching of binary descriptors can be efficiently done by comparing their Hamming distance as opposed to 
+ * Euclidean distance used for floating-point descriptors.
+ * 
+ * For comparing binary descriptors in OpenCV, use FLANN + LSH index or Brute Force + Hamming distance.
+ * 
+ * @BRUTEFORCE: 
+ *  - Hamming Distance: https://en.wikipedia.org/wiki/Hamming_distance
+ * @FLANN:
+ * - https://docs.opencv.org/3.4/d5/d6f/tutorial_feature_flann_matcher.html
+ * - https://docs.opencv.org/3.4/d5/dde/tutorial_feature_description.html
+ */
+
+/* Global Variables */
+// Choose the Feature/Matcher Method:
+const char* feature_matcher_enum2str[] = {
+    "ORB + BruteForce (Hamming)", 
+    "ORB + FlannBased", // FIXME: Can I use this?
+    "SIFT + BruteForce (Norm L2)",
+    "SIFT + FlannBased"
+};
+int feature_matcher_selected = 4;
+
 double matches_lower_bound = 30.0;
 
+/* =========== */
+/*  Functions  */
+/* =========== */
 void find_features_matches(const Mat &image1, const Mat &image2, vector<KeyPoint> &keypoints1, vector<KeyPoint> &keypoints2, vector<DMatch> &goodMatches, int nfeatures, bool verbose){
-    //--- Initialization
+    /* Initialization */
     Mat descriptors1, descriptors2;
 
-    #ifdef OPENCV3
-//        cout << "'OpenCV3' selected." << endl << endl;
-        // Ptr<FeatureDetector> detector = SIFT::create(500); // TODO: Terminar
-        // Ptr<DescriptorExtractor> descriptor = SIFT::create(500);
-        Ptr<FeatureDetector> detector = ORB::create(nfeatures);
-        Ptr<DescriptorExtractor> descriptor = ORB::create(nfeatures);
-        Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
-        // Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("FlannBased");
-    #else
-//        cout << "'OpenCV2' selected." << endl << endl;
-        Ptr<FeatureDetector> detector = FeatureDetector::create ("ORB" );
-        Ptr<DescriptorExtractor> descriptor = DescriptorExtractor::create ("ORB" );
-        BFMatcher matcher(NORM_HAMMING);
-    #endif
+    /* --------------------- */
+    /*  Features Extraction  */
+    /* --------------------- */
+    Ptr<FeatureDetector> detector;
+    Ptr<DescriptorExtractor> descriptor;
+    Ptr<DescriptorMatcher> matcher;
+    string descriptorType = "";
+            
+    switch (feature_matcher_selected){
+        case 1:  // ORB + BruteForce-Hamming, Descriptor type: CV_8U
+            detector = ORB::create(nfeatures);
+            descriptor = ORB::create(nfeatures);
+            matcher = DescriptorMatcher::create("BruteForce-Hamming");
+            descriptorType = "binary-string";
+            break;
+        case 2: // FIXME ORB + FlannBased, Descriptor type: CV_8U
+            detector = ORB::create(500);
+            descriptor = ORB::create(500);
+            matcher = DescriptorMatcher::create("FlannBased");
+            descriptorType = "binary-string";
+            break;
+        case 3: // SIFT + BruteForce, Descriptor type: CV_32F
+            detector = SIFT::create(500);
+            descriptor = SIFT::create(500);
+            matcher = DescriptorMatcher::create("BruteForce");  // It uses Norm L2
+            descriptorType = "float-point";
+            break;
+        case 4: // SIFT + FlannBased, Descriptor type: CV_32F
+            detector = SIFT::create(500);
+            descriptor = SIFT::create(500);
+            matcher = DescriptorMatcher::create("FlannBased");
+            descriptorType = "float-point";
+            break;
+        default:
+            break;
+    }
 
-    //--- Step 1: Detect the position of the Oriented FAST keypoints (Corner Points)
+    cout << "-- Feature/Matcher selected: " << feature_matcher_enum2str[feature_matcher_selected-1] << endl;
+
+    //--- Step 1: Detect the position of the keypoints (Corner Points)
     Timer t1 = chrono::steady_clock::now();
     detector->detect(image1, keypoints1);
     detector->detect(image2, keypoints2);
     Timer t2 = chrono::steady_clock::now();
 
-    //--- Step 2: Calculate the BRIEF descriptors based on the position of Oriented FAST keypoints
+    //--- Step 2: Calculate the descriptors based on the position of keypoints
     descriptor->compute(image1, keypoints1, descriptors1);
     descriptor->compute(image2, keypoints2, descriptors2);
     Timer t3 = chrono::steady_clock::now();
@@ -66,46 +122,64 @@ void find_features_matches(const Mat &image1, const Mat &image2, vector<KeyPoint
     //cout << descriptors1 << endl;
     //cout << descriptors2 << endl;
 
-    //--- Step 3: Match the BRIEF descriptors of the two images using the Hamming distance
+    // cout << "descriptors1: " << descriptors1.type() << endl;
+    // cout << "descriptors2: " << descriptors2.type() << endl;
+    // cin.ignore();
+
+    /* ------------------- */
+    /*  Features Matching  */
+    /* ------------------- */
+    //--- Step 3: Match the descriptors of the two images using the appropriate matching metric (Hamming or Euclidean distances)
     vector<DMatch> matches;
-
-    // if(descriptors1.type()!=CV_32F) {
-    //     descriptors1.convertTo(descriptors1, CV_32F);
-    // }
-
-    // if(descriptors2.type()!=CV_32F) {
-    //     descriptors2.convertTo(descriptors2, CV_32F);
-    // }
+    vector<vector<DMatch>> knn_matches;
 
     Timer t4 = chrono::steady_clock::now();
-    matcher->match(descriptors1, descriptors2, matches);  // TODO: Ver como rodar aquele algoritmo de matching FLANN (Parece ser melhor quando tem-se muitos pontos)
+    if(descriptorType.compare("binary-string") == 0){
+        matcher->match(descriptors1, descriptors2, matches);
+    }else if(descriptorType.compare("float-point") == 0){
+        matcher->knnMatch(descriptors1, descriptors2, knn_matches, 2);
+    }
     Timer t5 = chrono::steady_clock::now();
 
-    //--- Step 4: Select correct matching (filtering)
-    // Calculate the min & max distances
-    double min_dist = 10000, max_dist = 0;
+    /* -------------------- */
+    /*  Features Filtering  */
+    /* -------------------- */
+    //--- Step 4: Correct matching selection
+    if(descriptorType.compare("binary-string") == 0){  // Hamming Distance
+        /* Calculate the min & max distances */
+        // Find the minimum and maximum distances between all matches, that is, the distance between the most similar and least similar two sets of points
+        double min_dist = 10000, max_dist = 0;
 
-    // Find the minimum and maximum distances between all matches, that is, the distance between the most similar and least similar two sets of points
-    Timer t6 = chrono::steady_clock::now();
-    for (int i = 0; i < descriptors1.rows; i++){
-        double dist = matches[i].distance;
-        if(dist < min_dist) min_dist = dist;
-        if(dist > max_dist) max_dist = dist;
-    }
+        Timer t6 = chrono::steady_clock::now();
+        for (int i = 0; i < descriptors1.rows; i++){
+            double dist = matches[i].distance;
+            if(dist < min_dist) min_dist = dist;
+            if(dist > max_dist) max_dist = dist;
+        }
 
-    // Rule of Thumb: When the distance between the descriptors is greater than 2 times the min distance, we treat the matching as wrong.
-    // But sometimes the min distance could be very small, set an experience value of 30 as the lower bound.
-//    vector<DMatch> goodMatches;
+        /* Perform Filtering */
+        // Rule of Thumb: When the distance between the descriptors is greater than 2 times the min distance, we treat the matching
+        // as wrong. But sometimes the min distance could be very small, set an experience value of 30 as the lower bound.
+    //    vector<DMatch> goodMatches;
 
-    Timer t7 = chrono::steady_clock::now();
-    for (int i=0; i<descriptors1.rows; i++){
-        // cout << matches[i].distance << endl;
-        if (matches[i].distance <= max(2*min_dist, matches_lower_bound)){
-            goodMatches.push_back(matches[i]);
+        Timer t7 = chrono::steady_clock::now();
+        for (int i=0; i<descriptors1.rows; i++){
+            // cout << matches[i].distance << endl;
+            if (matches[i].distance <= max(2*min_dist, matches_lower_bound)){
+                goodMatches.push_back(matches[i]);
+            }
+        }
+        Timer t8 = chrono::steady_clock::now();
+    }else if(descriptorType.compare("float-point") == 0){  // Lowe's ratio test
+        // Filter matches using the Lowe's ratio test
+        const float ratio_threshold = 0.7f;
+        for(size_t i=0; i < knn_matches.size(); i++){
+            if(knn_matches[i][0].distance < ratio_threshold * knn_matches[i][1].distance){
+                goodMatches.push_back(knn_matches[i][0]);
+            }
         }
     }
-    Timer t8 = chrono::steady_clock::now();
-
+    
     //--- Step 5: Visualize the Matching result
     Mat outImage1, outImage2;
     Mat image_matches;
@@ -122,18 +196,19 @@ void find_features_matches(const Mat &image1, const Mat &image2, vector<KeyPoint
         printElapsedTime("ORB Features Extraction: ", t1, t3);
         printElapsedTime(" | Oriented FAST Keypoints detection: ", t1, t2);
         printElapsedTime(" | BRIEF descriptors calculation: ", t2, t3);
-        cout << "-- Number of detected keypoints1: " << keypoints1.size() << endl;
+        cout << "\n-- Number of detected keypoints1: " << keypoints1.size() << endl;
         cout << "-- Number of detected keypoints2: " << keypoints2.size() << endl << endl;
 
         printElapsedTime("ORB Features Matching: ", t4, t5);
         cout << "-- Number of matches: " << matches.size() << endl << endl;
-        
-        printElapsedTime("ORB Features Filtering: ", t6, t8);
-        printElapsedTime(" | Min & Max Distances Calculation: ", t6, t7);
-        printElapsedTime(" | Filtering by Hamming Distance: ", t7, t8);
-        cout << "-- Min dist: " << min_dist << endl;
-        cout << "-- Max dist: " << max_dist << endl;
-        cout << "-- Number of good matches: " << goodMatches.size() << endl << endl;
+
+        // TODO: Uncomment lines        
+        // printElapsedTime("ORB Features Filtering: ", t6, t8);
+        // printElapsedTime(" | Min & Max Distances Calculation: ", t6, t7);
+        // printElapsedTime(" | Filtering by Hamming Distance: ", t7, t8);
+        // cout << "-- Min dist: " << min_dist << endl;
+        // cout << "-- Max dist: " << max_dist << endl;
+        // cout << "-- Number of good matches: " << goodMatches.size() << endl << endl;
     }
 
     cout << "In total, we get " << goodMatches.size() << "/" << matches.size() << " good pairs of feature points." << endl << endl;
