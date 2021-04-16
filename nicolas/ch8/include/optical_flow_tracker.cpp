6eb5741b214d951546b703ddccdcf200b110263b
nicolasrosa <nicolas.rosa@hotmail.com>
2021-04-16 15:48:20 -0300

    [Mod] optical_flow_tracker created

---
+++ nicolas/ch8/include/optical_flow_tracker.cpp
@@ -0,0 +1,28 @@
+/* =========== */
+/*  Libraries  */
+/* =========== */
+/* System Libraries */
+#include <iostream>
+#include <chrono>
+#include <dirent.h>
+#include <string>
+#include <system_error>
+
+/* OpenCV Libraries */
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/opencv.hpp>
+
+/* Custom Libraries */
+#include "../../common/libUtils_basic.h"
+#include "../../common/libUtils_eigen.h"
+#include "../../common/libUtils_opencv.h"
+
+using namespace std;
+using namespace cv;
+
+/* ============================== */
+/*  Class Methods Implementation  */
+/* ============================== */
